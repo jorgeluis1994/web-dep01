495d1bef7a8127515b1755a3faffae11074632d9
jorge <jorgeluis199454@gmail.com>
2025-09-17 21:11:44 -0500

    register

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'we-forms-mag';
+
+   openedSection: number | null = 1; // Abre el login por defecto
+
+  // Puedes añadir métodos si quieres manejar validaciones
+  login() {
+    alert('Login ejecutado');
+  }
+
+  register() {
+    alert('Registro ejecutado');
+  }
 }
