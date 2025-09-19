e79a05fd9e5660fc0c21d5457f0d3a788b686510
jorge <jorgeluis199454@gmail.com>
2025-09-18 21:14:18 -0500

    ngprime

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import Material from '@primeuix/themes/material';
+
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+        options: {
+          darkModeSelector: 'body.light-mode' // forzamos un selector claro
+        }
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
